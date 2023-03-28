1faaf477cfb23707091f669f88d7443d5dcb554f
Anastasiia Sokolova <anastasiiasokolova@Anastasiias-MacBook-Pro.local>
2023-03-28 01:04:19 +0100

    Tokens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,23 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_request, only: [:create]
+  skip_before_action :authenticate_request, only: [:create]
 
-    def create
-      user = User.new(user_params)
-  
-      if user.save
-        render json: { status: 'User created successfully' }, status: :created
-      else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      # Generate a new JWT token
+      auth_token = JsonWebToken.encode({ user_id: user.id })
+
+      # Return the auth token in the response
+      render json: { auth_token: auth_token, status: 'User created successfully' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
